ef9c2a7e9b0d5c5d99b41fb476d1eb6d430af285
LutSerg <5QGJAZCqesdHUvk>
2023-04-09 12:47:18 +0300

    homework

--- src/main/java/gmail/asteroster/Main.java
+++ src/main/java/gmail/asteroster/Main.java
@@ -1,2 +1,48 @@
-package gmail.asteroster;public class Main {
+package gmail.asteroster;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5,
+                b = 10,
+                c = 15;
+        //арифметические операторы
+        System.out.println("a + c: " + (a + c));
+        System.out.println("a - c: " + (a - c));
+        System.out.println("c * b: " + (c * b));
+        System.out.println("c / b: " + (c / b));
+        System.out.println("c % b: " + (c % b));
+
+        //логические операторы
+        boolean d;
+        //false или true
+        d = (a == c);
+        System.out.println("a==c: " + d);
+        d = (a < c);
+        System.out.println("a < c: " + d);
+        d = (a > c);
+        System.out.println("a > c: " + d);
+
+        d = (a < b) & (a > c);
+        System.out.println("(a < b) & (a > c) :" + d);
+        d = (a < b) && (a > c);
+        System.out.println("(a < b) & (a > c) :" + d);
+        d = (a != b) && (a < c);
+        System.out.println("(a != b) && (a < c) :" + d);
+        d = !((a > b) && (a > c));
+        System.out.println("!((a > b) && (a > c)) :" + d);
+        d = (a > b) || (a < c);
+        System.out.println("(a > b) || (a < c) :" + d);
+        d = (a != b) || (a < c);
+        System.out.println("(a > b) || (a < c) :" + d);
+
+        double g = 1.012D;
+        System.out.println("x: " + (g + 20));
+
+        int e = Integer.MAX_VALUE;
+        System.out.println("e: " + (e + 1));
+
+        int f = Integer.MIN_VALUE;
+        System.out.println("f: "+ (f - 1));
+
+    }
 }
